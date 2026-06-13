8d41db1314817df80e129f79ea259c44b3478223
AvanishSalunke <avanishsalunke16@gmail.com>
2026-06-13 09:02:37 +0000

    restore fitlm old test

--- inst/fitlm.m
+++ inst/fitlm.m
@@ -175,6 +175,126 @@ function [modelspec, nv_args] = lm_split_args (remaining)
   endif
 endfunction
 
+%!demo
+%! y =  [ 8.706 10.362 11.552  6.941 10.983 10.092  6.421 14.943 15.931 ...
+%!        22.968 18.590 16.567 15.944 21.637 14.492 17.965 18.851 22.891 ...
+%!        22.028 16.884 17.252 18.325 25.435 19.141 21.238 22.196 18.038 ...
+%!        22.628 31.163 26.053 24.419 32.145 28.966 30.207 29.142 33.212 ...
+%!        25.694 ]';
+%! X = [1 1 1 1 1 1 1 1 2 2 2 2 2 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5]';
+%!
+%! mdl = fitlm (X, y, 'linear', 'CategoricalVars', 1)
+
+%!demo
+%! popcorn = [5.5, 4.5, 3.5; 5.5, 4.5, 4.0; 6.0, 4.0, 3.0; ...
+%!            6.5, 5.0, 4.0; 7.0, 5.5, 5.0; 7.0, 5.0, 4.5];
+%! brands = {'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'};
+%! popper = {'oil', 'oil', 'oil'; 'oil', 'oil', 'oil'; 'oil', 'oil', 'oil'; ...
+%!           'air', 'air', 'air'; 'air', 'air', 'air'; 'air', 'air', 'air'};
+%!
+%! T = table (brands(:), popper(:), 'VariableNames', {'brands', 'popper'});
+%! mdl = fitlm (T, popcorn(:), 'interactions')
+
+%!test
+%! y =  [ 8.706 10.362 11.552  6.941 10.983 10.092  6.421 14.943 15.931 ...
+%!        22.968 18.590 16.567 15.944 21.637 14.492 17.965 18.851 22.891 ...
+%!        22.028 16.884 17.252 18.325 25.435 19.141 21.238 22.196 18.038 ...
+%!        22.628 31.163 26.053 24.419 32.145 28.966 30.207 29.142 33.212 ...
+%!        25.694 ]';
+%! X = [1 1 1 1 1 1 1 1 2 2 2 2 2 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5]';
+%! fitlm (X, y, 'CategoricalVars', 1);
+%! fitlm (X, y, 'constant', 'CategoricalVars', 1);
+%! fitlm (X, y, 'linear', 'CategoricalVars', 1);
+%! mdl = fitlm (X, y, 'linear', 'CategoricalVars', 1);
+%! assert (mdl.Coefficients.Estimate(1), 10, 1e-04);
+%! assert (mdl.Coefficients.Estimate(2), 7.99999999999999, 1e-09);
+%! assert (mdl.Coefficients.Estimate(3), 8.99999999999999, 1e-09);
+%! assert (mdl.Coefficients.Estimate(4), 11.0001428571429, 1e-09);
+%! assert (mdl.Coefficients.Estimate(5), 19.0001111111111, 1e-09);
+%! assert (mdl.Coefficients.SE(1), 1.01775379540949, 1e-09);
+%! assert (mdl.Coefficients.SE(2), 1.64107868458008, 1e-09);
+%! assert (mdl.Coefficients.SE(3), 1.43932122062479, 1e-09);
+%! assert (mdl.Coefficients.SE(4), 1.48983900477565, 1e-09);
+%! assert (mdl.Coefficients.SE(5), 1.3987687997822, 1e-09);
+%! assert (mdl.Coefficients.tStat(1), 9.82555903510687, 1e-09);
+%! assert (mdl.Coefficients.tStat(2), 4.87484242844031, 1e-09);
+%! assert (mdl.Coefficients.tStat(3), 6.25294748040552, 1e-09);
+%! assert (mdl.Coefficients.tStat(4), 7.38344399756088, 1e-09);
+%! assert (mdl.Coefficients.tStat(5), 13.5834536158296, 1e-09);
+%! assert (mdl.Coefficients.pValue(2), 2.85812420217862e-05, 1e-12);
+%! assert (mdl.Coefficients.pValue(3), 5.22936741204002e-07, 1e-06);
+%! assert (mdl.Coefficients.pValue(4), 2.12794763209106e-08, 1e-07);
+%! assert (mdl.Coefficients.pValue(5), 7.82091664406755e-15, 1e-08);
+
+%!test
+%! popcorn = [5.5, 4.5, 3.5; 5.5, 4.5, 4.0; 6.0, 4.0, 3.0; ...
+%!            6.5, 5.0, 4.0; 7.0, 5.5, 5.0; 7.0, 5.0, 4.5];
+%! brands = bsxfun (@times, ones (6, 1), [1, 2, 3]);
+%! popper = bsxfun (@times, [1; 1; 1; 2; 2; 2], ones (1, 3));
+%! X = [brands(:), popper(:)];
+%! mdl = fitlm (X, popcorn(:), 'interactions', 'CategoricalVars', [1, 2]);
+%! assert (mdl.Coefficients.Estimate(1),  5.66666666666667, 1e-09);
+%! assert (mdl.Coefficients.Estimate(2), -1.33333333333333, 1e-09);
+%! assert (mdl.Coefficients.Estimate(3), -2.16666666666667, 1e-09);
+%! assert (mdl.Coefficients.Estimate(4),  1.16666666666667, 1e-09);
+%! assert (mdl.Coefficients.Estimate(6), -0.333333333333334, 1e-09);
+%! assert (mdl.Coefficients.Estimate(7), -0.166666666666667, 1e-09);
+%! assert (mdl.Coefficients.SE(1), 0.215165741455965, 1e-09);
+%! assert (mdl.Coefficients.SE(2), 0.304290309725089, 1e-09);
+%! assert (mdl.Coefficients.SE(3), 0.304290309725089, 1e-09);
+%! assert (mdl.Coefficients.SE(4), 0.304290309725089, 1e-09);
+%! assert (mdl.Coefficients.SE(6), 0.43033148291193, 1e-09);
+%! assert (mdl.Coefficients.SE(7), 0.43033148291193, 1e-09);
+%! assert (mdl.Coefficients.tStat(1),  26.3362867542108,   1e-09);
+%! assert (mdl.Coefficients.tStat(2),  -4.38178046004138,  1e-09);
+%! assert (mdl.Coefficients.tStat(3),  -7.12039324756724,  1e-09);
+%! assert (mdl.Coefficients.tStat(4),   3.83405790253621,  1e-09);
+%! assert (mdl.Coefficients.tStat(6),  -0.774596669241495, 1e-09);
+%! assert (mdl.Coefficients.tStat(7),  -0.387298334620748, 1e-09);
+%! assert (mdl.Coefficients.pValue(1), 5.49841502258254e-12, 1e-09);
+%! assert (mdl.Coefficients.pValue(2), 0.000893505495903642, 1e-09);
+%! assert (mdl.Coefficients.pValue(3), 1.21291454302428e-05, 1e-09);
+%! assert (mdl.Coefficients.pValue(4), 0.00237798044119407,  1e-09);
+%! assert (mdl.Coefficients.pValue(6), 0.453570536021938,    1e-09);
+%! assert (mdl.Coefficients.pValue(7), 0.705316781644046,    1e-09);
+%! brands = {'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'; ...
+%!           'Gourmet', 'National', 'Generic'};
+%! popper = {'oil', 'oil', 'oil'; 'oil', 'oil', 'oil'; 'oil', 'oil', 'oil'; ...
+%!           'air', 'air', 'air'; 'air', 'air', 'air'; 'air', 'air', 'air'};
+%! T = table (brands(:), popper(:), 'VariableNames', {'brands', 'popper'});
+%! mdl = fitlm (T, popcorn(:), 'interactions');
+
+%!test
+%! load carsmall
+%! X = [Weight, Horsepower, Acceleration];
+%! fitlm (X, MPG, 'constant');
+%! mdl = fitlm (X, MPG, 'linear');
+%! assert (mdl.Coefficients.Estimate(1),  47.9767628118615,     1e-09);
+%! assert (mdl.Coefficients.Estimate(2),  -0.00654155878851796, 1e-09);
+%! assert (mdl.Coefficients.Estimate(3),  -0.0429433065881864,  1e-09);
+%! assert (mdl.Coefficients.Estimate(4),  -0.0115826516894871,  1e-09);
+%! assert (mdl.Coefficients.SE(1), 3.87851641748551,            1e-09);
+%! assert (mdl.Coefficients.SE(2), 0.00112741016370336,         1e-09);
+%! assert (mdl.Coefficients.SE(3), 0.0243130608813806,          1e-09);
+%! assert (mdl.Coefficients.SE(4), 0.193325043113178,           1e-09);
+%! assert (mdl.Coefficients.tStat(1),  12.369874881944,         1e-09);
+%! assert (mdl.Coefficients.tStat(2),  -5.80228828790225,       1e-09);
+%! assert (mdl.Coefficients.tStat(3),  -1.76626492228599,       1e-09);
+%! assert (mdl.Coefficients.tStat(4),  -0.0599128364487485,     1e-09);
+%! assert (mdl.Coefficients.pValue(1), 4.89570341688996e-21,    1e-09);
+%! assert (mdl.Coefficients.pValue(2), 9.87424814144e-08,       1e-09);
+%! assert (mdl.Coefficients.pValue(3), 0.0807803098213114,      1e-09);
+%! assert (mdl.Coefficients.pValue(4), 0.952359384151778,       1e-09);
+
 
 %!shared X, y, yl, T1, T2, T3
 %! X  = [1 2; 3 4; 5 6];
